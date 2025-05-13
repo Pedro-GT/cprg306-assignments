c738acc01ca6a22e5208dbc93ed84467a479d259
Pedro Gomes <pedrodetoledoh@gmail.com>
2025-05-13 10:01:25 -0600

    Finished assinment for week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+
+    return(
+        
+        <div>
+            <h3>Name: Pedro Henrique Gomes De Toledo</h3>
+            <Link href="https://github.com/Pedro-GT/cprg306-assignments">GitHub</Link>
+        </div>
+
+    );
+
+}
